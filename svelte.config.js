26c94b5fb5244ecf3ca2a074df7f98274e994ac4
nightly5 <120316532+nightly5@users.noreply.github.com>
2023-04-24 10:36:16 +0530

    Initial commit
    Signed-off-by: nightly5 <120316532+nightly5@users.noreply.github.com>

---
+++ svelte.config.js
@@ -0,0 +1,16 @@
+import adapter from '@sveltejs/adapter-static';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+    preprocess: vitePreprocess(),
+
+    kit: {
+        adapter: adapter({
+            precompress: true,
+            strict: true
+        })
+    }
+};
+
+export default config;
